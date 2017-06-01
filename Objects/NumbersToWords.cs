0660f74d1755aaab2fbc76e04cd2c33f84e0c01f
Jun Fritz and Olena Kuchko <jun+olena@>
2017-06-01 16:51:02 -0700

    add function to put it all together.

--- Objects/NumbersToWords.cs
+++ Objects/NumbersToWords.cs
@@ -21,52 +21,73 @@ namespace NumbersToWords.Objects
       return Math.Ceiling(groupCount);
 
     }
-    public char [] GetGroup(string input)
+    public string GetGroup(string input)
     {
       char [] arrayString = input.ToCharArray();
       char [] array = new char [3];
-      for (int i=0; i<3; i++)
-       {
-        array[i] = arrayString[i];
+      for (int i=0, j=input.Length-3; i<3; i++, j++)
+      {
+        array[i] = arrayString[j];
       }
-      return array;
+      Console.WriteLine(array);
+      string s = new string(array);
+      return s;
     }
 
-    public string ConvertGroup (int groupInput)
+    public string ConvertGroup (string numberSection)
     {
-      string result = "";
+      int groupInput = Convert.ToInt32(numberSection);
+      string sectionResult = "";
       int hundreds = groupInput/100;
       int tens = groupInput % 100;
       int ones = groupInput % 10;
-      Console.WriteLine(result);
+      Console.WriteLine(sectionResult);
 
-      // return uniqueSpecial[tens];
+      // converts "hundreds" place
       Console.WriteLine(tens + "this one");
       if (hundreds != 0)
       {
-        result += uniqueSpecial[hundreds] + " hundred ";
+        sectionResult += uniqueSpecial[hundreds] + " hundred ";
       }
       Console.WriteLine(tens);
-      if (tens != 0 && uniqueSpecial.ContainsKey(tens))
-      {
-        result += uniqueSpecial[tens];
-        Console.WriteLine(tens);
-        Console.WriteLine(result);
-        return result;
-      }
+        //Tests "tens" place
+        if (tens != 0 && uniqueSpecial.ContainsKey(tens))
+        {
+          sectionResult += uniqueSpecial[tens];
+          Console.WriteLine(tens);
+          Console.WriteLine(sectionResult);
+          return sectionResult;
+        }
         else if (tens/10 == 2 || tens/10 == 3 || tens/10 == 5 || tens/10 == 8)
         {
-          result += " " + uniqueSpecial[tens-ones];
+          sectionResult += " " + uniqueSpecial[tens-ones];
         }
         else
         {
-          result += uniqueSpecial[tens/10] + "ty ";
+          sectionResult += uniqueSpecial[tens/10] + "ty ";
         }
+
       if (ones != 0)
       {
-        result += uniqueSpecial[ones];
+        sectionResult += uniqueSpecial[ones];
       }
-      Console.WriteLine(result);
+      Console.WriteLine(sectionResult);
+      return sectionResult;
+    }
+    public string ConvertWholeNumber(string input)
+    {
+      string result = "";
+      while (input != "")
+      {
+        Console.WriteLine("While");
+        string section = GetGroup(input);
+        Console.WriteLine(section);
+        result += ConvertGroup(section);
+        input.Remove(input.Length-3, 3);
+
+      }
+      Console.WriteLine(input);
+      Console.WriteLine(GetGroup(input));
       return result;
     }
   }
--- Tests/NumbersToWordsTest.cs
+++ Tests/NumbersToWordsTest.cs
@@ -7,24 +7,31 @@ namespace NumbersToWords
 {
   public class NumberToWordsTests
   {
+    // [Fact]
+    // public void TestFunction()
+    // {
+    //   Conversion newConversion = new Conversion();
+    //   Assert.Equal(3, newConversion.ConvertNum(1000000));
+    // }
+    // [Fact]
+    // public void TestGetGroup()
+    // {
+    //   Conversion newConversion = new Conversion();
+    //   char [] test = new char [] {'2', '3', '4'};
+    //   Assert.Equal(test, newConversion.GetGroup("1234"));
+    // }
+    // [Fact]
+    // public void TestConvertGroup()
+    // {
+    //   Conversion newConversion = new Conversion();
+    //   Assert.Equal("eight", newConversion.ConvertGroup("999"));
+    // }
     [Fact]
-    public void TestFunction()
+    public void Test()
     {
       Conversion newConversion = new Conversion();
-      Assert.Equal(3, newConversion.ConvertNum(1000000));
-    }
-    [Fact]
-    public void TestGetGroup()
-    {
-      Conversion newConversion = new Conversion();
-      char [] test = new char [] {'1', '0', '0'};
-      Assert.Equal(test, newConversion.GetGroup("1000"));
-    }
-    [Fact]
-    public void TestConvertGroup()
-    {
-      Conversion newConversion = new Conversion();
-      Assert.Equal("eight", newConversion.ConvertGroup(122));
+      Assert.Equal("six", newConversion.ConvertWholeNumber("987654"));
+      Console.WriteLine(newConversion.ConvertWholeNumber("987654"));
     }
   }
 }
